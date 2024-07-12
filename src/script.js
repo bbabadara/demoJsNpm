ab6a6a5d602a389d5bbf1e9ea32b4b0dd15aab7f
bbabadara <bbabadara@gmail.com>
2024-07-12 00:34:24 +0000

    afficher les dettes d'un client -V2 ajout pagination

--- src/script.js
+++ src/script.js
@@ -4,6 +4,8 @@ const inputNom=document.querySelector("#inputNom")
 const inputPrenom=document.querySelector("#inputPrenom")
 const inputAdresse=document.querySelector("#inputAdresse")
 const tBody=document.querySelector("#tBody")
+const pagination=document.querySelector("#pagination")
+const elementPerPage=2
 
 // Declaration tableau 
 let clients = [];
@@ -17,6 +19,8 @@ fetchDatas().then(data => {
 
 document.addEventListener("DOMContentLoaded", async (event) => {
   await fetchDatas();
+
+   //Events
   inputTel.addEventListener("change",function(){
     // alert(inputTel.value)
    const client=findClientByTel(inputTel.value.trim());
@@ -24,7 +28,7 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     inputTel.classList.remove("is-invalid")
     inputTel.classList.add("is-valid")
     inputTel.nextElementSibling.textContent = "";
-     tBody.innerHTML=generateTbody(client.dette)
+    generatePagination(client.dette,pagination,tBody,generateTbody)
   }
     else{
       inputTel.classList.remove("is-valid")
@@ -33,9 +37,9 @@ document.addEventListener("DOMContentLoaded", async (event) => {
     }      
    
  })
-// tBody.innerHTML=generateTbody(clients.dette)
 
- //Events
+
+
 
 
 
@@ -72,11 +76,43 @@ function generateTbody(elements) {
 function generateTr(element) {
   return `<tr"> 
         <td>${element.date}</td>
-        <td>${element.montant} </td>
-        <td >${element.verse}</td>
-        <td>${element.montant-element.verse}</td>
+        <td>${element.montant}  </td>
+        <td >${element.verse} </td>
+        <td>${element.montant-element.verse} </td>
       </tr>`
 }
 
 
 
+function getDatasPaginate(tab,start,elementPage){
+    let firstPosition=(start-1)*elementPage;
+    let lastPosition=firstPosition+elementPage;
+    return {
+      datas:tab.slice(firstPosition,lastPosition),
+      page:Math.ceil(tab.length/elementPage)
+    };
+   }
+    
+   function generatePagination(allDatas,elementUl,elementTboby,generateTbody){
+  const {datas,page}=this.getDatasPaginate(allDatas,1,elementPerPage);
+  let html=''
+    // html+=`<li class="page-item" ><button class="page-link" href="#">Previous</button></li>`;
+  for (let i = 1; i <=page; i++) {
+    html+=`<li class="page-item ${i==1?'active':''}" data-number="${i}"><button class="page-link" >${i}</button></li>` 
+  }
+  
+//   html+=`<li class="page-item" ><button class="page-link" href="#">Next</button></li>`;
+  
+  elementUl.innerHTML= html
+  elementTboby.innerHTML=generateTbody(datas)
+  const itemsLi=elementUl.querySelectorAll(".page-item")
+  itemsLi.forEach((item)=>{
+    item.addEventListener("click",(e)=>{
+        elementUl.querySelector(".active").classList.remove("active")
+      item.classList.add("active")
+      let {datas}=this.getDatasPaginate(allDatas,parseInt(item.dataset.number),elementPerPage)
+      elementTboby.innerHTML=generateTbody(datas)
+  })
+  })
+  
+   }
